682f2788a68c0ea263465322f40eb87f05e8c792
taufik91 <taufik@logique.co.id>
2018-01-28 19:50:22 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -3,15 +3,14 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('airhorner').then(function(cache) {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/index.html?homescreen=1',
-                '/css/bootstrap.min.css',
-                '/css/font-awesome.min.css',
-                '/css/main.css',
-                '/js/jquery.min.js',
-                '/js/jquery.ui.min.js',
-                '/js/bootstrap.min.js'
+                'index.html',
+                'index.html?homescreen=1',
+                'css/bootstrap.min.css',
+                'css/font-awesome.min.css',
+                'css/main.css',
+                'js/jquery.min.js',
+                'js/jquery.ui.min.js',
+                'js/bootstrap.min.js'
             ]);
         })
     );
