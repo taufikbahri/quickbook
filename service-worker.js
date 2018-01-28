35dc094fd7af76641ddc0119a6e5927bd8b957ea
taufik91 <taufik@logique.co.id>
2018-01-28 20:53:52 +0700

    login page

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,5 @@
 importScripts('cache-polyfill.js');
-// self.addEventListener('install', function(e) {
-//     e.waitUntil(
-//         caches.open('airhorner').then(function(cache) {
-//             return cache.addAll([
-//                 'index.html',
-//                 'index.html?homescreen=1',
-//                 'css/bootstrap.min.css',
-//                 'css/font-awesome.min.css',
-//                 'css/main.css',
-//                 'js/jquery.min.js',
-//                 'js/jquery.ui.min.js',
-//                 'js/bootstrap.min.js'
-//             ]);
-//         })
-//     );
-// });
+
 var cacheName = 'quickbook';
 
 self.addEventListener('install', (event) => {
